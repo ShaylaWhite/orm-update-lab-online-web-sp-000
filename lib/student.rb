d7e5b541fb1998934009b6d1b1f232f09ce1e19e
Shayla White <shayladwhite1@gmail.com>
2020-01-17 03:16:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,30 +1,32 @@
 require_relative "../config/environment.rb"
 
 class Student
-  
-attr_accessor :name, :grade
-attr_reader :id
+  attr_accessor :name, :grade, :id
+    def initialize(name,grade,id=nil)
+      @name = name
+      @grade = grade
+      @id = id
+    end
 
-  def initialize(id=nil, name, grade)
-    @id = id
-    @name = name
-    @grade = grade
-  end
-  
   def self.create_table
-    sql =  <<-SQL
+      sql =  <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
         )
         SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql)   
   end
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)   
   end
   
   def save
@@ -41,8 +43,7 @@ attr_reader :id
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   end
-
-
+  
   def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
@@ -53,6 +54,7 @@ attr_reader :id
   	new_student = self.new(row[1],row[2],row[0])  
   	new_student 
   end
+  
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
@@ -65,5 +67,4 @@ attr_reader :id
       self.new_from_db(row)
     end.first
   end
-  
 end
