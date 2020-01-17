00c0d52d22b38b12f939356cfed66591efcc7ca0
Shayla White <shayladwhite1@gmail.com>
2020-01-17 02:51:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,10 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-  def initialize 
-    
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
   end
 
 
